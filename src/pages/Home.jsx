49f64544ee9e1b658be2a55e7f7473cf7a9ab64c
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2025-05-18 17:28:23 -0600

    adjusted 1 liner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Home() {
     return (
         <div style={{ paddingTop: '80px' }}>
-            <h1>Welcome to DeFi Factoring</h1>
-            <h2>Accelerate cash flow through decentralized factoring – bridging invoices to liquidity</h2>
+            <h1>DeFi Factoring</h1>
+            <h2>Get credits using your invoices as collateral</h2>
         </div>
     );
 }
